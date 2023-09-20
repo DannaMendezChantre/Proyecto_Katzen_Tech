c119066da6c2b3cddee45f281562ebc2bbe5181b
DannaMendezChantre <danzmendez@soy.sena.edu.co>
2023-09-19 21:13:17 -0500

    html sanbra 2.5

---
+++ Sanbra2.5/listaclientes.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/listapro.css">
+    <title>Inicio</title>
+    <script src="js/listapro.js"></script>
+</head>
+<body>
+    <?php
+    $link = new PDO("mysql:host=localhost;dbname=sanbra11", "root", "");
+    ?>
+    <!-- Banner -->
+    <div class="banner">
+        <a href="index.html"><span class="katzen">Katzen</span><span class="tech">Tech</span></a>
+
+    </div>
+    <div class="cen">
+        <a class="agg" href="ingresarcliente.html">Volver</a>&nbsp&nbsp&nbsp&nbsp
+        <div class="fil">
+            <input type="text" id="buscar" placeholder="Buscar...">
+        </div>
+        <h1>Clientes</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Tipo de documento</th>
+                    <th>Id</th>
+                    <th>Numero documento</th>
+                    <th>Nombre empresa</th>
+                    <th>Teléfono</th>
+                    <th>Dirección</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($link->query("SELECT * FROM cliente") as $row) { ?>
+                    <tr>
+                        <td><?php echo $row["pk_fk_Tipo_documento"] ?></td>
+                        <td><?php echo $row["Id_cliente"] ?></td>
+                        <td><?php echo $row["Numero_documento"] ?></td>
+                        <td><?php echo $row["Nombre_empresa"] ?></td>
+                        <td><?php echo $row["Telefono"] ?></td>
+                        <td><?php echo $row["Direccion"] ?></td>
+                        <td><a class="agg" href="#">Editar</a></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
---
+++ Sanbra2.5/listafacturas.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/listapro.css">
+    <title>Inicio</title>
+    <script src="js/listapro.js"></script>
+</head>
+<body>
+    <?php
+    $link = new PDO("mysql:host=localhost;dbname=sanbra11", "root", "");
+    ?>
+    <!-- Banner -->
+    <div class="banner">
+        <a href="index.html"><span class="katzen">Katzen</span><span class="tech">Tech</span></a>
+    </div>
+    <div class="cen">
+        <a class="agg" href="index.html">Inicio</a>&nbsp&nbsp&nbsp&nbsp
+        <a class="agg" href="factura.html">Volver</a>
+        <div class="fil">
+            <input type="text" id="buscar" placeholder="Buscar...">
+        </div>
+        <h1>Facturas</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID Factura</th>
+                    <th>ID Administrador</th>
+                    <th>ID Cliente</th>
+                    <th>Fecha de Factura</th>
+                    <th>Total de Factura</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($link->query("SELECT * FROM factura") as $row) { ?>
+                <tr>
+                    <td><?php echo $row["Id_factura"] ?></td>
+                    <td><?php echo $row["Administrador_id"] ?></td>
+                    <td><?php echo $row["Cliente_id"] ?></td>
+                    <td><?php echo $row["Fecha_factura"] ?></td>
+                    <td><?php echo $row["Total_factura"] ?></td>
+                    <td><a class="agg" href="#">Editar</a></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
